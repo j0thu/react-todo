58728c6a884873fb2b40f2657600d4b0b3598dc4
j0thu <jonathanben215@gmail.com>
2020-07-30 23:00:14 +0530

    New Todo

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is my all new Todo App, My first ever React App</p>
+        </React.Fragment>    
+    )
+}
+export default About;
